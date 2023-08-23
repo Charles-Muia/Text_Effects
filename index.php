b725fbe26c2dd4f5eafec3e9fa1c8be6ca9b64d6
Charles-Muia <Charles.K.Muia@gmail.com>
2023-08-23 11:39:29 +0300

    Text-Effect

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Text Animation</title>
+
+<!-----FAVICON--------------------------------------------------------------------------------------------------------->
+
+    <link rel="icon" href="Img/ADiDW_3.png"
+
+<!-----CSS STYLESHEET-------------------------------------------------------------------------------------------------->
+
+    <link rel="stylesheet" href="style.css">
+
+<!-----GOOGLE FONTS---------------------------------------------------------------------------------------------------->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<!-----VIDEO CLIP------------------------------------------------------------------------------------------------------>
+
+        <video src="V_Clip/Particles.mp4" muted="" autoplay=""></video>
+
+<!-----TEXT FOR EFFECT------------------------------------------------------------------------------------------------->
+
+        <h3>ano<span>the</span>r day in web <span>dev</span>elopment</h3>
+
+</body>
+</html>
+
+
